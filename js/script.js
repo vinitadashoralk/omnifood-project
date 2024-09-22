33de0d179ed2e56c7d2b42a38d8bd4b1b0d997f9
vinitadashoralk <vinitadashora.lk@gmail.com>
2024-09-22 23:51:15 +0530

    remove sticky nav when hero is visible

--- js/script.js
+++ js/script.js
@@ -56,13 +56,17 @@ const obs = new IntersectionObserver(
     const ent = entries[0];
     console.log(ent);
     if (ent.isIntersecting === false) {
-      document.querySelector(".header").classList.add("sticky");
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
     }
   },
   {
     // In the viewpoert
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroE1);
